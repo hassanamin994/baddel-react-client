fd046df7eaa7a7fd1680bd16d460c7a853c25ac0
Hassan Amin <hassan94924@gmail.com>
2017-12-07 22:27:48 +0200

    implement productsCard with model attrs

--- src/components/common/productCard.js
+++ src/components/common/productCard.js
@@ -1,22 +1,27 @@
 import React from 'react';
-
+import { Link } from 'react-router';
 
 class ProductCard extends React.Component {
 
     render() {
+        const product = this.props.product;
+        console.log(product)
+        
+        if(!product.images || product.images.length == 0 ) {
+            product.images[0] = '/assets/img/a.jpg';
+        }
+
         return (
             <div>
                 <div className="thumbnail">
-                    <a href="product_details.html" className="overlay"></a>
-                    <a className="zoomTool" href="product_details.html" title="add to cart"><span className="icon-search"></span> QUICK VIEW</a>
-                    <a href="product_details.html"><img src="assets/img/a.jpg" alt=""/></a>
+                    <Link to={'/products/' + product._id }><img src={product.images[0]} alt=""/></Link>
                     <div className="caption cntr">
-                        <p>Manicure & Pedicure</p>
-                        <p><strong> $22.00</strong></p>
-                        <h4><a className="shopBtn" href="#" title="add to cart"> Add to cart </a></h4>
+                        <p><strong>{product.title || product.name || '' }</strong></p>
+                        <p>{product.price || ''}</p>
+                        <h4><Link className="shopBtn" to={'/products/' +product._id} > View Product </Link></h4>
                         <div className="actionList">
-                            <a className="pull-left" href="#">Add to Wish List </a> 
-                            <a className="pull-left" href="#"> Add to Compare </a>
+                            <h6>{product.trade_with && product.trade_with.length > 0 && 'Trade with'}</h6>
+                            {product.trade_with.map(item => <p>{item}</p> )}
                         </div> 
                         <br className="clr" />
                     </div>
